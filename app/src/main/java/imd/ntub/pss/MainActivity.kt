48b2ca426d75fb1ae32ff4e5b56600ede14e6669
RemiuOWO <dean951209@gmail.com>
2025-02-27 18:10:22 +0800

    0227

---
+++ app/src/main/java/imd/ntub/pss/MainActivity.kt
@@ -0,0 +1,11 @@
+package imd.ntub.pss
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
